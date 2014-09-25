f359a55f2a0ce49990410e4e6351a44f021e9ae3
N3bTcx <nick.n3b@gmail.com>
2014-09-25 17:41:36 +0200

    Trying to add everything in one method
    But I'm stuck. I have always one value on my array and just one group is shown.

--- bin/app.rb
+++ bin/app.rb
@@ -5,26 +5,31 @@ set :public_folder, 'public'
 set :views, "views"
 
 def new_random_groups(numgroup)
-    studentname = IO.readlines('public/names.txt').map{ |element| element.chomp}.shuffle
-	numgroup = 1 #Comment ne pas mettre cette valeur?
-	numstudent = studentname.length
+	studentname = IO.readlines('public/names.txt').shuffle
+	# studentname = IO.readlines('public/names.txt').map{ |element| element.chomp}.shuffle
 	groups = (1..numgroup).zip(*studentname.each_slice(numgroup))
-	groups.each do |index, *items|
-    	items.compact! #remove nils
-    	#puts "Group #{index+1} with #{items.length} students: \n - #{items.join("\n - ")}"
-	end
-	studentname.shuffle
+	if numgroup == 1
+		groups.each do |*items|
+	    	items.compact! #remove nils
+	    	return "<ul><h2>Pas de groupe mais #{studentname.length} élèves:</h2>#{items.join("<br /><li>")}"
+	    end
+	else 
+		groups.each do |index, *items|
+    		items.compact! #remove nils
+    		return "#{items.join("<br />")}"
+    		# return "<ul><h5>Groupe #{index+1} avec #{items.length} élèves:</h5><br /><li>#{items.join("<br /><li>")}"
+   	end
+   end
 end
 
 
 get '/' do
-    @result = new_random_groups(1)
-  	erb :hello_form
+	@result = new_random_groups(1)
+	erb :hello_form
 end
 
 post '/hello/' do
-    numgroup = params[:numgroup].to_i || 1
-    @result = new_random_groups(@numgroup)
-    @numgroup = numgroup
-    erb :index, :locals => {'numgroup' => numgroup}
+	numgroup = params[:numgroup].to_i
+	@result = new_random_groups(numgroup)
+	erb :index, :locals => {'numgroup' => numgroup}
 end

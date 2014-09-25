c52db14d318f33269011c7c0a724ad5c9677245f
N3bTcx <nick.n3b@gmail.com>
2014-09-25 18:31:56 +0200

    Still issues to show properly array and groups
    Don't know what's happen with my return. I'm stuck.

--- bin/app.rb
+++ bin/app.rb
@@ -1,27 +1,9 @@
 require 'sinatra'
+require './new_random_groups'
 
 set :static, true
 set :public_folder, 'public'
 set :views, "views"
-
-def new_random_groups(numgroup)
-	studentname = IO.readlines('public/names.txt').shuffle
-	# studentname = IO.readlines('public/names.txt').map{ |element| element.chomp}.shuffle
-	groups = (1..numgroup).zip(*studentname.each_slice(numgroup))
-	if numgroup == 1
-		groups.each do |*items|
-	    	items.compact! #remove nils
-	    	return "<ul><h2>Pas de groupe mais #{studentname.length} élèves:</h2>#{items.join("<br /><li>")}"
-	    end
-	else 
-		groups.each do |index, *items|
-    		items.compact! #remove nils
-    		return "#{items.join("<br />")}"
-    		# return "<ul><h5>Groupe #{index+1} avec #{items.length} élèves:</h5><br /><li>#{items.join("<br /><li>")}"
-   	end
-   end
-end
-
 
 get '/' do
 	@result = new_random_groups(1)

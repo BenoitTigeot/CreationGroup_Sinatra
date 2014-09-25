4fc15f2ffcf8475853db00ac223e6ace7c3ac809
N3bTcx <nick.n3b@gmail.com>
2014-09-25 15:14:43 +0200

    Improve views, add list in all views. Working forms
    Nearly good. Just have to implement the method.

--- bin/app.rb
+++ bin/app.rb
@@ -4,17 +4,26 @@ set :static, true
 set :public_folder, 'public'
 set :views, "views"
 
-get '/' do
-    redirect '/index.html'
+def new_random_groups
+
+    studentname = ["Damien", "Rafik", "Benoit", "Anne-Marie", "Faz", "Son", "Stephane", "Mylene", "Abdrahmane", "Nolan", "Man", "Houda", "Mouna", "Guillaume", "Fanta", "Myriam", "Genevieve", "Semia", "Djibril", "Alexandra", "Guibril", "Mehdi", "Aurelie" ]
+	   
+	numgroup = 4# We add to_i otherwise we get a string
+	numstudent = studentname.length
+	studentname.each_slice(numgroup).with_index do |items,index|
+    	puts "Group #{index+1} with #{items.length} students: \n - #{items.join("\n - ")}"
+	end
+	studentname.shuffle
 end
 
-get '/hello/' do
-    erb :hello_form
+
+get '/' do
+    #redirect '/index.html'
+    @result = new_random_groups
+  	erb :hello_form
 end
 
 post '/hello/' do
-    greeting = params[:greeting] || "Hi There"
-    name = params[:name] || "Nobody"
-
-    erb :index, :locals => {'greeting' => greeting, 'name' => name}
+    numgroup = params[:numgroup].to_i || 1
+    erb :index, :locals => {'numgroup' => numgroup}
 end
